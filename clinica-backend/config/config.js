5fb0c333308f9ae5ce8732c009d2b7bec25b4699
lino jose <ljeq08@gmail.com>
2025-12-22 19:00:19 -0500

    add frontend URL to CORS

--- clinica-backend/config/config.js
+++ clinica-backend/config/config.js
@@ -11,21 +11,21 @@ const config = {
   },
 
   cors: {
-    origin: process.env.NODE_ENV === 'production' 
-      ? [
-          process.env.FRONTEND_URL,
-          'https://clinica-frontend.onrender.com'  // Ajusta con tu URL real
-        ]
-      : [
-          'http://localhost:5173',
-          'http://127.0.0.1:5173',
-          'http://localhost:5174',
-          'http://127.0.0.1:5174',
-          'http://localhost:5500',
-          'http://127.0.0.1:5500'
-        ],
-    credentials: true
-  },
+  origin: process.env.NODE_ENV === 'production' 
+    ? [
+        'https://clinica-front-nira.onrender.com',
+        process.env.FRONTEND_URL
+      ]
+    : [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+      ],
+  credentials: true
+},
 
   rateLimit: {
     windowMs: 15 * 60 * 1000,
